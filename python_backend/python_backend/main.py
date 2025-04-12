38cefd21da017c57e9829bdd78007c78e1cb4e89
Derin Onder Eren <derinondereren@gmail.com>
2025-04-11 23:42:38 -0300

    feat: make portfolio a multi-container application using docker compose. Using caddy as the webserver

--- python_backend/python_backend/main.py
+++ python_backend/python_backend/main.py
@@ -3,18 +3,18 @@ from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 
 from python_backend.routers import location, spotify
+from python_backend.lib.config_helper import Config
 
+config = Config()
 
 app = FastAPI()
 
 app.include_router(spotify.router)
 app.include_router(location.router)
 
-allowed_origins = ["http://localhost:3000"]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=allowed_origins,
+    allow_origins=config["api"]["allowed_origins"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
