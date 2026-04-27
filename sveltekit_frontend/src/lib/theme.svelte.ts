c395be5fadd1f7b799f2c539e5a26dffdd6e34d2
d3r1n <derinondereren@gmail.com>
2026-04-27 02:29:16 -0300

    change project structure and reset frontend for a rewrite

--- sveltekit_frontend/src/lib/theme.svelte.ts
+++
@@ -1,8 +0,0 @@
-export const theme = $state({
-	current: "wireframe",
-	toggle() {
-		this.current = this.current === "wireframe" ? "black" : "wireframe";
-		localStorage.setItem("theme", this.current);
-		document.documentElement.setAttribute("data-theme", this.current);
-	},
-});
--- sveltekit_frontend/src/routes/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import "../styles/app.css";
-
-	let { children } = $props();
-</script>
-
-<div class="w-full h-dvh">
-	{@render children()}
-</div>
