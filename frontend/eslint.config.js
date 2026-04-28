2afaf9adc87046d29265de44786a06920d48a426
d3r1n <derinondereren@gmail.com>
2026-04-27 21:20:53 -0300

    frontend setup

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,26 +1,26 @@
-import svelte from "eslint-plugin-svelte";
-import prettier from "eslint-config-prettier";
-import ts from "typescript-eslint";
-import globals from "globals";
+import svelte from 'eslint-plugin-svelte';
+import prettier from 'eslint-config-prettier';
+import ts from 'typescript-eslint';
+import globals from 'globals';
 
 export default [
-  // Base TypeScript recommended rules
-  ...ts.configs.recommended,
-  // Svelte recommended rules
-  ...svelte.configs["flat/recommended"],
-  // Prettier config (turns off conflicting rules)
-  prettier,
-  // Svelte + Prettier specific integration
-  ...svelte.configs["flat/prettier"],
-  {
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-      },
-    },
-  },
-  {
-    ignores: ["build/", ".svelte-kit/", "dist/"],
-  },
+	// Base TypeScript recommended rules
+	...ts.configs.recommended,
+	// Svelte recommended rules
+	...svelte.configs['flat/recommended'],
+	// Prettier config (turns off conflicting rules)
+	prettier,
+	// Svelte + Prettier specific integration
+	...svelte.configs['flat/prettier'],
+	{
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.node
+			}
+		}
+	},
+	{
+		ignores: ['build/', '.svelte-kit/', 'dist/']
+	}
 ];
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
-	import './layout.css';
+	import '../styles/app.css';
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+
+<div id="viewport-container" class="flex w-screen justify-center">
+	<div id="content" class="w-[60%] pt-48">
+		{@render children()}
+	</div>
+</div>
